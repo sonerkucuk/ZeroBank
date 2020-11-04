58e881923c2d2f0f51b80b8e078c93de6c3654ed
sonerkucuk <sonerkucuk@msn.com>
2020-11-04 13:13:39 +0100

    yeni dosya ekle

---
+++ src/test/java/hadibakalim.java
@@ -0,0 +1,7 @@
+public class hadibakalim {
+    //red means it is untracked
+    //green means it is under staging area
+    //grey in .idea folder means it is ignored
+    //when the color is normal, it means it is commited no changes
+
+}
